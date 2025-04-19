e6515605ec8e13a7df54c114bbfc978cb4f44807
Guilherme Ferreira <guil.ferreiram@gmail.com>
2025-04-19 16:51:37 -0300

    feat(PROD-2): exclude default pages

--- Product.App.Client/obj/Debug/net8.0/Product.App.Client.AssemblyInfo.cs
+++ Product.App.Client/obj/Debug/net8.0/Product.App.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product.App.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc73171ce54a9338bcc63b384431059019515dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product.App.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product.App.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
